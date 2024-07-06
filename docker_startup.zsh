b4c2e5db69f57c54fa990b0f833f409d00032897
CarlDegio <2536890272@qq.com>
2024-07-06 16:56:20 +0800

    change zed file mapping

--- docker_startup.zsh
+++ docker_startup.zsh
@@ -4,7 +4,8 @@ docker run -it \
 -e DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix \
 -v ~/robot_space:/root/ros2_ws \
 -v /dev:/dev --privileged \
--v /tmp/zed_ai/:/usr/local/zed/resources/ \
+-v ~/zed/zed_ai/:/usr/local/zed/resources/ \
+-v ~/zed/zed_settings/:/usr/local/zed/settings/ \
 --network host \
 --runtime=nvidia \
 --ipc=host --pid=host \
